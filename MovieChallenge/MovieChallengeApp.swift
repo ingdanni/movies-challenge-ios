d598d3c9c3e4a597b52a628ce81a2e324e6c6802
Danny Narvaez <ing.danni@gmail.com>
2021-12-15 13:13:40 -0600

    Initial commit

---
+++ MovieChallenge/MovieChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieChallengeApp.swift
+//  MovieChallenge
+//
+//  Created by Danny Narvaez on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
