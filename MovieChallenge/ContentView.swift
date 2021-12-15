a9462f96fd9c8804f89e4fb9edb815a03ea3cc62
Danny Narvaez <ing.danni@gmail.com>
2021-12-15 13:16:42 -0600

    feat: initial setup

--- MovieChallenge/ContentView.swift
+++ MovieChallenge/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Movie challenge")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
